72b43e268a8a64a634b6eead585fcc5221c63f43
iHamsterball <ihamsterball@gmail.com>
2015-07-04 15:12:19 +0800

    重构主要函数

--- Login/LoginNetConnect.cpp
+++ Login/LoginNetConnect.cpp
@@ -1,6 +1,52 @@
 #include "stdafx.h"
 #include "LoginNetConnect.h"
 
+//网页数据请求函数
+void LoginNetConnect::Request(CHttpConnection* pServer, CHttpFile* pf,
+	CString Method, CString RequestPath, CString Referer, CString Host,
+	CStringA data)
+{
+	//拼接头部
+	pf = pServer->OpenRequest((Method == _T("POST")) ? CHttpConnection::HTTP_VERB_POST : CHttpConnection::HTTP_VERB_GET, RequestPath);
+	pf->AddRequestHeaders(_T("Accept: text/html, application/xhtml+xml, */*"));
+	pf->AddRequestHeaders(_T("Referer: ") + Referer);
+	pf->AddRequestHeaders(_T("Accept-Language: zh-Hans-CN,zh-Hans;q=0.8,en-GB;q=0.5,en;q=0.3"));
+	pf->AddRequestHeaders(_T("User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"));
+	pf->AddRequestHeaders(_T("Content-Type: application/x-www-form-urlencoded"));
+	pf->AddRequestHeaders(_T("Host: ") + Host);
+	pf->AddRequestHeaders(_T("DNT: 1"));
+	pf->AddRequestHeaders(_T("Connection: Keep-Alive"));
+	pf->AddRequestHeaders(_T("Cache-Control: no-cache"));
+
+	pf->SendRequestEx(data.GetLength());
+	pf->WriteString((LPCTSTR)data.GetBuffer());
+	pf->EndRequest();
+
+	DWORD statusCode = 0;
+	if (!pf->QueryInfoStatusCode(statusCode) || statusCode != HTTP_STATUS_OK)
+	{
+		CString e;
+		e.Format(_T("网页响应代码%d\n"), statusCode);
+		MessageBox(0, e, _T("错误"), 0);
+	}
+
+	// 以下为读取数据
+	cache = _T(""); 
+	char str[1024];
+	while (pf->ReadString((LPTSTR)str, 1024))   //读取网页数据 
+	{
+		cache += str;
+	}
+
+	//关闭HttpFile
+	if (pf != NULL)
+	{
+		pf->Close();
+		delete pf;
+		pf = NULL;
+	}
+}
+
 // 模拟IE11登陆北林登陆网页
 void LoginNetConnect::OnBjfuLogin(
 	CStringA &username, // username
@@ -17,25 +63,13 @@ void LoginNetConnect::OnBjfuLogin(
 
 	CString ServerName = _T("202.204.122.1");
 	INTERNET_PORT nPort = 80;  //port
+	pServer = session.GetHttpConnection(ServerName, nPort); 
+	pf = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, _T("/"));
 
 	CStringA temp = "username=" + username + "&password=" + password + "&ip=" + ip + "&action=" + action;
 
-	pServer = session.GetHttpConnection(ServerName, nPort);
-	pf = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, _T("/"));
-	//pf->SendRequestEx(data.GetLength());
-	//pf->WriteString((LPCTSTR)data.GetBuffer());
-	pf->SendRequestEx(temp.GetLength());
-	pf->WriteString((LPCTSTR)temp.GetBuffer());
-	pf->EndRequest();
-
-
-	char str[1024];
-	while (pf->ReadString((LPTSTR)str, 1024))   //读取网页数据 
-	{
-		cache += str;
-	}
-	//MessageBox(cache, _T("网页信息"), 0);
-	
+	Request(pServer, pf, _T("GET"), _T("/"), _T(""), _T(""), temp);
+	//Package -1
 	//读取IP地址
 	{
 		int start, end;
@@ -46,68 +80,28 @@ void LoginNetConnect::OnBjfuLogin(
 		ip = cache.Mid(start + 1, end - start - 1);
 	}
 
-	if (pf != NULL)
-	{
-		pf->Close();
-		delete pf;
-		pf = NULL;
-	}//POST /checkLogin.jsp -package 1//in order to get Cookie
-
-
 	CString CookieData;
 	//获取Cookie
 	session.GetCookie(_T("http://202.204.122.1/checkLogin.jsp"), _T("JSESSIONID"), CookieData);
 	int tmp = GetLastError();
+	//设置Cookie
 	session.SetCookie(_T("http://202.204.122.1"), _T(""), CookieData);
 	
-	//获取IP地址后才能构造提交数据
+
+	//获取IP地址(Package 1)后才能构造提交数据
+	//POST /checkLogin.jsp -package 2 
+	//Login to system
 	CStringA data = "username=" + username + "&password=" + password + "&ip=" + ip + "&action=" + action;
 
-	//拼接头部
-	pf = pServer->OpenRequest(CHttpConnection::HTTP_VERB_POST, _T("/checkLogin.jsp"));
-	pf->AddRequestHeaders(_T("Accept: text/html, application/xhtml+xml, */*"));
-	pf->AddRequestHeaders(_T("Referer: http://202.204.122.1/index.jsp"));
-	pf->AddRequestHeaders(_T("Accept-Language: zh-Hans-CN,zh-Hans;q=0.8,en-GB;q=0.5,en;q=0.3"));
-	pf->AddRequestHeaders(_T("User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"));
-	pf->AddRequestHeaders(_T("Content-Type: application/x-www-form-urlencoded"));
-	pf->AddRequestHeaders(_T("Host: 202.204.122.1"));
-	//pf->AddRequestHeaders(_T("Content-Length: 88"));
-	pf->AddRequestHeaders(_T("DNT: 1"));
-	pf->AddRequestHeaders(_T("Connection: Keep-Alive"));
-	pf->AddRequestHeaders(_T("Cache-Control: no-cache"));
-
-	//pf->AddRequestHeaders(_T("Cookie: ") + CookieData);
-
-	//strcpy_s(data_, (char*)data.GetBuffer(data.GetLength()));
-	pf->SendRequestEx(data.GetLength());
-	pf->WriteString((LPCTSTR)data.GetBuffer());
-	pf->EndRequest();
-
-	DWORD statusCode = 0;
-	if (!pf->QueryInfoStatusCode(statusCode) || statusCode != HTTP_STATUS_OK)
-	{
-		CString e;
-		e.Format(_T("网页响应代码%d\n"), statusCode);
-		MessageBox(0, e, _T("错误"), 0);
-	}
-	DWORD ptrLen = 100 * 1024;
-
-	// 以下为读取数据
-	cache = _T("");
-	while (pf->ReadString((LPTSTR)str, 1024))   //读取网页数据 
-	{
-		cache += str;
-	}
-	//MessageBox(NULL, cache, _T("网页信息"), 0);
+	Request(pServer, pf, 
+		_T("POST"), 
+		_T("/checkLogin.jsp"), 
+		_T("http://202.204.122.1/index.jsp"), 
+		_T("202.204.122.1"), data);
+	//检查用户名密码
 	if (cache.Find(_T("用户名或密码不正确")) != -1)
 	{
 		ConnectStatus = _T("密码错误");
-		if (pf != NULL)
-		{
-			pf->Close();
-			delete pf;
-			pf = NULL;
-		}
 		if (pServer != NULL)
 		{
 			pServer->Close();
@@ -115,53 +109,17 @@ void LoginNetConnect::OnBjfuLogin(
 			pServer = NULL;
 		}
 		session.Close();
-
 		return;
 	}
-	if (pf != NULL)
-	{
-		pf->Close();
-		delete pf;
-		pf = NULL;
-	}
-	//POST /checkLogin.jsp -package 2 //Login to system
-
-
-	//pServer = session.GetHttpConnection(ServerName, nPort);
-	//拼接头部
-	pf = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, _T("/user/index.jsp?ip=") + (CString)ip + _T("&action=") + ((action == "disconnect") ? _T("disconnect") : _T("connect")));
-	pf->AddRequestHeaders(_T("Accept: text/html, application/xhtml+xml, */*"));
-	pf->AddRequestHeaders(_T("Referer: http://202.204.122.1/index.jsp"));
-	pf->AddRequestHeaders(_T("Accept-Language: zh-Hans-CN,zh-Hans;q=0.8,en-GB;q=0.5,en;q=0.3"));
-	pf->AddRequestHeaders(_T("User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"));
-	pf->AddRequestHeaders(_T("DNT: 1"));
-	pf->AddRequestHeaders(_T("Host: 202.204.122.1"));
-	pf->AddRequestHeaders(_T("Cookie: ") + CookieData);
-	pf->AddRequestHeaders(_T("Connection: Keep-Alive"));
-	pf->AddRequestHeaders(_T("Cache-Control: no-cache"));
-	//pf->SendRequest(NULL, 0, data.GetBuffer(0), data.GetLength());
-
-	pf->SendRequestEx(data.GetLength());
-	pf->WriteString((LPCTSTR)data.GetBuffer());
-	pf->EndRequest();
-
-	if (!pf->QueryInfoStatusCode(statusCode) || statusCode != HTTP_STATUS_OK)
-	{
-		CString e;
-		e.Format(_T("网页响应代码%d\n"), statusCode);
-		MessageBox(0, e, _T("错误"), 0);
-	}
-
-	// 以下为读取数据
-	cache = _T("");
-	while (pf->ReadString((LPTSTR)str, 1024))   //读取网页数据 
-	{
-		cache += str;
-	}
-	//MessageBox(cache, _T("网页信息"), 0);
 
 	//通过cache中的src确定下一个数据包请求的地址
 	//不同用户，userid不同
+	//GET /user/index.jsp?ip="IP Address"&action=connect -package 3
+	Request(pServer, pf,
+		_T("GET"),
+		_T("/user/index.jsp?ip=") + (CString)ip + _T("&action=") + ((action == "disconnect") ? _T("disconnect") : _T("connect")),
+		_T("http://202.204.122.1/index.jsp"),
+		_T("202.204.122.1"), data);
 	CString RequestURL;
 	{
 		int start, end;
@@ -171,66 +129,12 @@ void LoginNetConnect::OnBjfuLogin(
 		//MessageBox(cache, _T("网页信息"), 0);
 	}
 
-	if (pf != NULL)
-	{
-		pf->Close();
-		delete pf;
-		pf = NULL;
-	}
-	//GET /user/index.jsp?ip=118.228.171.91&action=connect -package 3 
-	//to get RequestURL which is different in each user
-
-
-	//pServer = session.GetHttpConnection(ServerName, nPort);
-	//拼接头部
-	pf = pServer->OpenRequest(CHttpConnection::HTTP_VERB_GET, _T("/user/") + RequestURL);
-	pf->AddRequestHeaders(_T("Accept: text/html, application/xhtml+xml, */*"));
-	pf->AddRequestHeaders(_T("Referer: http://202.204.122.1/user/index.jsp?ip=") + (CString)ip + _T("&action") + ((action == "disconnect") ? _T("disconnect") : _T("connect")));
-	pf->AddRequestHeaders(_T("Accept-Language: zh-Hans-CN,zh-Hans;q=0.8,en-GB;q=0.5,en;q=0.3"));
-	pf->AddRequestHeaders(_T("User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko"));
-	pf->AddRequestHeaders(_T("Host: 202.204.122.1"));
-	pf->AddRequestHeaders(_T("DNT: 1"));
-	pf->AddRequestHeaders(_T("Connection: Keep-Alive"));
-	pf->AddRequestHeaders(_T("Cache-Control: no-cache"));
-	pf->AddRequestHeaders(_T("Cookie: ") + CookieData);
-	//pf->SendRequest(NULL, 0, data.GetBuffer(0), data.GetLength());
-
-	pf->SendRequestEx(data.GetLength());
-	pf->WriteString((LPCTSTR)data.GetBuffer());
-	pf->EndRequest();
-
-	if (!pf->QueryInfoStatusCode(statusCode) || statusCode != HTTP_STATUS_OK)
-	{
-		CString e;
-		e.Format(_T("网页响应代码%d\n"), statusCode);
-		MessageBox(0, e, _T("错误"), 0);
-	}
-
-	// 以下为读取数据
-	cache = _T("");
-	while (pf->ReadString((LPTSTR)str, 1024))   //读取网页数据 
-	{
-		cache += str;
-	}
-	//MessageBox(cache, _T("网页信息"), 0);
-
-	if (pf != NULL)
-	{
-		pf->Close();
-		delete pf;
-		pf = NULL;
-	}
-	if (pServer != NULL)
-	{
-		pServer->Close();
-		delete pServer;
-		pServer = NULL;
-	}
 	//GET /user/+RequestURL -package 4
-	//get required info
-
-	session.Close();
-
+	//收集需要的信息
+	Request(pServer, pf, _T("GET"),
+		_T("/user/") + RequestURL,
+		_T("http://202.204.122.1/user/index.jsp?ip=") + (CString)ip + _T("&action") + ((action == "disconnect") ? _T("disconnect") : _T("connect")),
+		_T("202.204.122.1"), data);
 	if (action != "disconnect")
 	{
 		//处理cache以获取需要的信息
@@ -243,6 +147,15 @@ void LoginNetConnect::OnBjfuLogin(
 		DataGather(BasicDataExceed, _T("超出基础流量"), _T("FF0000"), _T("&nbsp;GB"));//超出基础流量
 		DataGather(ExceedDataFee, _T("超流量费用"), _T("FF0000"), _T("</font>"));//超流量费用
 	}
+
+	//断开与服务器的连接
+	if (pServer != NULL)
+	{
+		pServer->Close();
+		delete pServer;
+		pServer = NULL;
+	}
+	session.Close();
 }
 void LoginNetConnect::DataGather(CString &data)
 {
--- Login/LoginNetConnect.h
+++ Login/LoginNetConnect.h
@@ -12,12 +12,14 @@ public:
 		CStringA &password, //password
 		CStringA &ip, //ip
 		CStringA &action //action
-		);
+		);//TODO: 里面的那个长函数有时间必须找个方法重写 -- 已重写
+
 	inline void Empty(){ Account = ""; Password = ""; }
 
 	//数据
 	CStringA Account;//账号
 	CStringA Password;//密码
+	int userid;//貌似有一个唯一的userid用于后续操作
 
 	CString ConnectStatus;//连接状态
 	double AccountBalance;//账户余额
@@ -27,6 +29,11 @@ public:
 	double BasicDataBalance;//剩余基础流量
 	double BasicDataExceed;//超出基础流量
 	double ExceedDataFee;//超流量费用
+
+	//内部网络数据请求函数
+	void Request(CHttpConnection* pServer, CHttpFile* pf,
+		CString Method, CString RequestPath, CString Referer, CString Host,
+		CStringA data);
 
 	//内部数据处理函数
 	void DataGather(CString &data);
